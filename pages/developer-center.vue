4363bc24e800ad3e9a526d883f3aa0c84f41c794
Breiden Busch <breiden.busch@gmail.com>
2019-12-05 10:55:04 -0700

    Add page for developer center

---
+++ pages/developer-center.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Developer Center Page
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { test: 'I am a test piece of data' }
+  }
+}
+</script>
