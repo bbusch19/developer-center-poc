77acab1e9a73d0697291cb74670577f1b613fbb3
Breiden Busch <breiden.busch@gmail.com>
2019-12-05 16:38:24 -0700

    Add initial files, added components and styles

---
+++ components/SplitContent.vue
@@ -0,0 +1,73 @@
+<template>
+  <section :style="{ backgroundImageStyles }">
+    <div class="content">
+      <p>FEATURED</p>
+      <img src="/images/logo-site-iq.png" />
+      <h2>Assess Location Risk in Seconds</h2>
+      <p>
+        Reduce underwriting overhead and make more informed decisions faster
+        than ever.
+      </p>
+      <ul>
+        <li>Build on the worlds’s Leading CAT Science</li>
+        <li>Supported by Key Third Party Insights</li>
+        <li>Customized for your Unique Risk Appetite</li>
+      </ul>
+      <ActionButton :classes="['ghost', 'primary']" />
+    </div>
+    <div class="image-container">
+      <img :src="image" />
+    </div>
+  </section>
+</template>
+
+<script>
+import ActionButton from '~/components/ActionButton'
+export default {
+  components: {
+    ActionButton
+  },
+  props: {
+    content: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      default: null
+    },
+    backgroundImage: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    backgroundImageStyles() {
+      return (
+        this.backgroundImage && {
+          backgroundImage: `url(${this.backgroundImage})`
+        }
+      )
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+section {
+  display: flex;
+  padding: 100px 80px;
+  background-color: grey;
+  color: white;
+
+  & > div {
+    flex-basis: 100%;
+  }
+}
+
+.image-container {
+  img {
+    width: 100%;
+  }
+}
+</style>
--- pages/developer-center.vue
+++ pages/developer-center.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="container">
-    <div>
-      <h1 class="title">
-        Developer Center Page
-      </h1>
-    </div>
+    <HeroSearch
+      heading="Developers Center"
+      subheading="Integrate with RMS. Easily build apps with our APIs."
+    />
+    <SplitContent
+      content="Hey there this is the content!"
+      image="/images/SiteIq-hero-image.png"
+    />
   </div>
 </template>
 
 <script>
+import HeroSearch from '~/components/HeroSearch'
+import SplitContent from '~/components/SplitContent'
+
 export default {
+  components: {
+    HeroSearch,
+    SplitContent
+  },
   data() {
     return { test: 'I am a test piece of data' }
   }
