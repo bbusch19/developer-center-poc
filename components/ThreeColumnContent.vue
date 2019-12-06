e46831c06e50a876d4f8f1c4fdb5b1869e4b2c70
tomgalpin <tomgalpin@gmail.com>
2019-12-06 12:39:07 -0500

    wic

---
+++ components/ThreeColumnContent.vue
@@ -0,0 +1,7 @@
+<template>
+  <p>tomtomt</p>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped></style>
--- pages/developer-center.vue
+++ pages/developer-center.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="container">
-    <div>
-      <h1 class="title">
-        Developer Center Page
-      </h1>
-    </div>
+    <ThreeColumnContent />
   </div>
 </template>
 
 <script>
+import ThreeColumnContent from '~/components/ThreeColumnContent'
+
 export default {
+  components: {
+    ThreeColumnContent
+  },
   data() {
     return { test: 'I am a test piece of data' }
   }
