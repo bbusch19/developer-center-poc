506f88baf1d4b375945a9a7d57e3cae30c915850
Breiden Busch <breiden.busch@gmail.com>
2019-12-09 16:17:19 -0700

    Add header components. Add images used in header

---
+++ components/BaseHeaderNavigation.vue
@@ -0,0 +1,103 @@
+<template>
+  <nav role="navigation">
+    <span v-for="item of navigationItems" :key="item.text" class="primary-link">
+      <span v-if="item.to">
+        <nuxt-link :to="item.to" exact>{{ item.text }}</nuxt-link>
+      </span>
+      <span v-else
+        >{{ item.text }} <img src="/images/icons/arrow-down.svg"
+      /></span>
+      <ul v-if="item.items" class="subnav">
+        <li v-for="subItem in item.items" :key="subItem.text">
+          <a v-if="subItem.to" :href="subItem.to">{{ subItem.text }}</a>
+        </li>
+      </ul>
+    </span>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navigationItems: [
+        {
+          to: '/',
+          text: 'Home'
+        },
+        {
+          to: null,
+          text: 'Events',
+          items: [{ to: '#', text: 'See Events' }]
+        },
+        {
+          to: null,
+          text: 'Products',
+          items: [{ to: '#', text: 'See Products' }]
+        },
+        {
+          to: '/learn',
+          text: 'Learn'
+        },
+        {
+          to: '/developer-center',
+          text: 'Developers'
+        },
+        {
+          to: '/support',
+          text: 'Support'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/css/colors.scss';
+
+nav {
+  display: flex;
+}
+
+.primary-link {
+  padding: 30px 13px;
+  font-weight: 500;
+
+  a,
+  span {
+    text-decoration: none;
+    color: $darker-text;
+
+    img {
+      display: inline-block;
+      vertical-align: bottom;
+      width: 15px;
+      height: 15px;
+      opacity: 0.6;
+    }
+  }
+
+  .nuxt-link-active {
+    color: $primary;
+    border-bottom: 3px solid $primary;
+  }
+
+  &:hover {
+    .subnav {
+      display: block;
+    }
+  }
+}
+
+.subnav {
+  display: none;
+  position: absolute;
+  padding: 0;
+  list-style-type: none;
+  background-color: white;
+  margin: 0;
+  padding: 20px;
+  box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.1);
+}
+</style>
