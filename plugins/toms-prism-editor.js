ecb2a12a2c672135a65ecbda485e76103845e04c
tomgalpin <tomgalpin@gmail.com>
2019-12-17 16:19:45 -0500

    cleanup

--- plugins/toms-prism-editor.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-
-import VuePrismJS from 'vue-prismjs'
-import 'prismjs'
-import 'prismjs/themes/prism.css'
-// import 'vue-prism-editor/dist/VuePrismEditor.css' // import the styles
-// import '~/assets/css/prism-code-editor.css'
-
-Vue.component('vue-prismjs', VuePrismJS)
