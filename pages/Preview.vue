3b0d4052185742c45d1ae3ee3c8426e5ea874bfb
Breiden Busch <breiden.busch@gmail.com>
2020-01-03 13:38:06 -0700

    Added prismic client helper

--- pages/Preview.vue
+++ pages/Preview.vue
@@ -5,17 +5,19 @@
 </template>
 
 <script>
-import Prismic from 'prismic-javascript'
+import { Client } from '~/prismicHelpers'
 import LinkResolver from '~/plugins/link-resolver.js'
-import PrismicConfig from '~/prismic.config.js'
 
 export default {
   name: 'Preview',
-  async asyncData({ query, redirect }) {
+  async asyncData({ req, query, redirect }) {
     const previewToken = query.token
-    const api = await Prismic.getApi(PrismicConfig.apiEndpoint)
     if (previewToken) {
-      const url = await api.previewSession(previewToken, LinkResolver, '/')
+      const url = await Client(req).previewSession(
+        previewToken,
+        LinkResolver,
+        '/'
+      )
       redirect(url)
     }
   }
